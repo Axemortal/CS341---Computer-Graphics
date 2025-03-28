c9c0e0a484e0172c3099c7cb4a26ec7f1672d461
Wu Yifan <94105227+Axemortal@users.noreply.github.com>
2025-03-28 11:42:02 +0100

    Task GL2.3: Gouraud Shading

--- GL2_meshes/src/shade_pervertex.frag.glsl
+++ GL2_meshes/src/shade_pervertex.frag.glsl
@@ -1,13 +1,11 @@
 precision mediump float;
 
 // #TODO GL2 setup varying
-// varying ...
+varying vec3 vertex_color;
 
-void main()
-{
+void main() {
 	/*
 	#TODO GL2.3: Gouraud lighting
 	*/
-	vec3 color = vec3(1., 0., 0.);
-	gl_FragColor = vec4(color, 1.); // output: RGBA in 0..1 range
+	gl_FragColor = vec4(vertex_color, 1.); // output: RGBA in 0..1 range
 }
